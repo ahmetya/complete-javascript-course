5c465909661c32049bbfacbecc15e6399345408f
Ahmet Yalcinkaya <ahmetyky@gmail.com>
2022-11-30 17:41:08 +0100

    Course Update

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -145,3 +145,136 @@ const checkWinner = function (avgDolphins, avgKoalas) {
 
 console.log(checkWinner(avgDolphins, avgKoalas));
 
+// Arrays and Object 2 important data structures
+
+const friends = ['Michale', 'Steven', 'Peter']; //literal syntax
+console.log(friends);
+
+const years = new Array(1991, 1986, 2008, 2020); //using array function
+console.log(years);
+console.log(friends[0]);
+console.log(friends.length); //length is the property of the array
+console.log(friends[friends.length - 1]); //expresion is a line that returns a value, inside bracket javascript expect a espression not a statement
+// change or mutate array
+friends[2] = 'Ahmet';
+console.log(friends);
+
+// when you declare array with const you can change, only primitive values are immutable.
+// but you can not do friends = ['Mehmet', 'Metin'];
+
+const firstName = 'ahmet';
+const ahmet = [firstName, 'Kubra', 2036 - 1986, friends];
+console.log(ahmet);
+
+const calcAgeArray = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsForArray = [1986, 1923, 2002, 2001];
+const age1 = calcAgeArray(yearsForArray[yearsForArray.length - 1]);
+console.log(calcAgeArray(yearsForArray[0]));
+console.log(age1);
+
+const calcAgeArrowForArray = (birthYear) => {
+    return 2037 - birthYear;
+}
+
+console.log(calcAgeArrowForArray(1986));
+
+const newLength = friends.push('Kubra'); // push adds to the end of the array.
+console.log(friends);
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('Emre'); // unshift add to the end of the array
+console.log(friends);
+
+// Remove from array
+// Pop removes item at the end of the array
+const popReturn = friends.pop(); // pop removes and returns the removed element of the array
+console.log(friends);
+console.log(popReturn);
+
+console.log(friends);
+const shiftReturn = friends.shift(); // shift removes the first value of the array and retunrs the removed value
+console.log(friends);
+console.log(shiftReturn);
+
+console.log(friends.indexOf('Ahmet'));
+
+
+friends.push(23);
+console.log(friends.includes('Ahmet')); // returns only true or false
+console.log(friends.includes('Zart')); // returns only true or false
+console.log('23'); //return false because this checks strict equality
+
+if (friends.includes('Ahmet')) {
+    console.log('you have a friend called ahmet');
+}
+
+
+const tipCalculatorArray = (billValue) => {
+
+    // let tip;
+    // if (billValue > 50 && billValue < 300) {
+    //     tip = billValue * 0.15;
+    // } else {
+    //     tip = billValue * 0.20;
+    // }
+    // return tip;
+
+    const tip = billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.20;
+    return tip;
+}
+
+const tipCalculatorArrayArrow = billValue => billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.20;
+
+console.log(tipCalculatorArray(100));
+
+const bills = new Array(125, 555, 44);
+const tips = new Array;
+const totals = new Array;
+console.log(bills);
+
+for (let i in bills) { // you can use anything for i for (bill in bills) it is just a index
+    console.log(bills[i]);
+    tips.push(tipCalculatorArray(bills[i]));
+    totals.push(tipCalculatorArray(bills[i]) + bills[i])
+}
+
+console.log(tips);
+console.log(totals);
+
+// const tips = new Array[tipCalculatorArray(bills[0]), tipCalculatorArray(bills[1]), tipCalculatorArray(bills[2])];
+
+const ahmetObject = {
+    firstName: 'Ahmet',
+    lastName: 'Yalcinkaya',
+    job: 'qa',
+    age: 36,
+    friends: ['Kubra', 'Mert', 'Emre'],
+    bestFriend: 'Emre'
+}
+
+console.log(ahmetObject);
+console.log(ahmetObject.lastName); // dot is the operator here
+console.log(ahmetObject['firstName']);
+const nameKey = 'Name';
+console.log(ahmetObject['first' + nameKey]); //in square brackets accepts expression so we can do this like it, this wont work with dot notation
+console.log(ahmetObject['last' + nameKey]);
+
+const interestedIn = 'age'
+// const interestedIn = prompt('What do you want to know about Ahmet? Choose between firstName, lastName, age, job and friends');
+console.log(ahmetObject[interestedIn]);
+
+if (ahmetObject[interestedIn]) {
+    console.log(ahmetObject[interestedIn]);
+} else {
+    console.log('Wrong request');
+}
+
+ahmetObject.location = 'Malta';
+ahmetObject['twitter'] = '@ykahmet';
+console.log(ahmetObject);
+
+console.log(`${ahmetObject.firstName} has ${ahmetObject.friends.length} friends and his best friends is ${ahmetObject.bestFriend} and his wife is ${ahmetObject.friends[0]}`);
